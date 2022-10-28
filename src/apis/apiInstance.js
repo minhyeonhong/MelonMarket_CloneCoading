20b030478a653acc686daedb3c70f66ea1d1a64d
임효진 <imhyojin@imhyojicBookPro.Davolink>
2022-10-29 00:20:10 +0900

    ADD : 회원가입 CSS디자인

--- src/apis/apiInstance.js
+++ src/apis/apiInstance.js
@@ -1,57 +1,50 @@
 import axios from "axios";
-import { setCookie, getCookie, delCookie } from "../cookie/cookie"
-
+import { setCookie, getCookie, delCookie } from "../cookie/cookie";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    //baseURL: process.env.REACT_APP_API_LOCAL_URL,
-    headers: {
-        "content-type": "application/json;charset=UTF-8",
-        accept: "application/json",
-        "Access_Token": getCookie("token"),
-        "Access-Control-Allow-Origin": "*",
-    },
-    withCredentials: true,
+  baseURL: process.env.REACT_APP_API_URL,
+  //baseURL: process.env.REACT_APP_API_LOCAL_URL,
+  headers: {
+    "content-type": "application/json;charset=UTF-8",
+    accept: "application/json",
+    Access_Token: getCookie("token"),
+    "Access-Control-Allow-Origin": "*",
+  },
+  withCredentials: true,
 });
 
 export const loginApis = {
-    //로그인
-    loginAX: (loginInfo) => instance.post(`/auth/login`, loginInfo),
-    //회원가입
-    joinAX: (joinInfo) => instance.post(`/auth/signup`, joinInfo),
-    //회원가입 아이디 중복 체크
-    loginCheckAX: (email) => instance.post(`/auth/idCheck`, email),
-    //회원가입 낙네임 중복 체크
-    loginCheckAX: (name) => instance.post(`/auth/nameCheck`, name),
+  //로그인
+  loginAX: (loginInfo) => instance.post(`/auth/login`, loginInfo),
+  //회원가입
+  joinAX: (joinInfo) => instance.post(`/auth/signup`, joinInfo),
+  //회원가입 아이디 중복 체크
+  loginCheckAX: (email) => instance.post(`/auth/idCheck`, email),
+  //회원가입 낙네임 중복 체크
+  loginCheckAX: (name) => instance.post(`/auth/nameCheck`, name),
 
+  //게시글 삭제
+  getDeletePostAX: (id) => instance.delete(`/detail/delete?boardId=${id}`),
 
-
-    //게시글 삭제
-    getDeletePostAX: (id) => instance.delete(`/detail/delete?boardId=${id}`),
-
-    //마이페이지 계정 수정 페이지
-    putUserInfoAX: (userinfo) => instance.put("/my/update", userinfo),
-
+  //마이페이지 계정 수정 페이지
+  putUserInfoAX: (userinfo) => instance.put("/my/update", userinfo),
 };
 
 export const contentsApis = {
-    //로그인
-    loginAX: (loginInfo) => instance.post(`/auth/login`, loginInfo),
-    //회원가입
-    joinAX: (joinInfo) => instance.post(`/auth/signup`, joinInfo),
-    //회원가입 아이디 중복 체크
-    loginCheckAX: (email) => instance.post(`/auth/idCheck`, email),
-    //회원가입 낙네임 중복 체크
-    loginCheckAX: (name) => instance.post(`/auth/nameCheck`, name),
+  //로그인
+  loginAX: (loginInfo) => instance.post(`/auth/login`, loginInfo),
+  //회원가입
+  joinAX: (joinInfo) => instance.post(`/auth/signup`, joinInfo),
+  //회원가입 아이디 중복 체크
+  loginCheckAX: (email) => instance.post(`/auth/idCheck`, email),
+  //회원가입 낙네임 중복 체크
+  loginCheckAX: (name) => instance.post(`/auth/nameCheck`, name),
 
+  //게시글 삭제
+  getDeletePostAX: (id) => instance.delete(`/detail/delete?boardId=${id}`),
 
-
-    //게시글 삭제
-    getDeletePostAX: (id) => instance.delete(`/detail/delete?boardId=${id}`),
-
-    //마이페이지 계정 수정 페이지
-    putUserInfoAX: (userinfo) => instance.put("/my/update", userinfo),
-
+  //마이페이지 계정 수정 페이지
+  putUserInfoAX: (userinfo) => instance.put("/my/update", userinfo),
 };
 
 //이런식으로 쓴다
@@ -69,6 +62,5 @@ export const contentsApis = {
 //             alert(err);
 //         });
 // };
-
 
 export default loginApis;
--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -1,15 +1,15 @@
-import React from "react"
-import styled, { css } from "styled-components"
+import React from "react";
+import styled, { css } from "styled-components";
 
 const Button = (props) => {
-    return (
-        <StButton {...props} disabled={props.disabled}>
-            {props.children}
-        </StButton>
-    )
-}
+  return (
+    <StButton {...props} disabled={props.disabled}>
+      {props.children}
+    </StButton>
+  );
+};
 
-export default Button
+export default Button;
 
 const StButton = styled.button`
   padding: 10px;
@@ -21,48 +21,48 @@ const StButton = styled.button`
   font-size: 1rem;
   line-height: 1.4;
   border: 0;
-  color: #fd5c63;
-  border-radius: 10px;
-  box-shadow: 0px 2px 10px #e1cccd;
+  color: ${(props) => props.bgColor};
+  border-radius: 0px;
+  box-shadow: 0px 2px 10px #5688b9;
   background-color: white;
   cursor: pointer;
-  &:hover {
-    background-color: #fd5c63;
+  /* &:hover {
+    background-color: #4267b2;
     border: 0;
     color: white;
-  }
+  } */
   ${({ size }) => {
-        switch (size) {
-            case "full":
-                return css`
+    switch (size) {
+      case "full":
+        return css`
           width: 100%;
-        `
-            case "large":
-                return css`
+        `;
+      case "large":
+        return css`
           width: 50%;
-        `
-            case "medium":
-                return css`
+        `;
+      case "medium":
+        return css`
           width: 200px;
           height: 40px !important;
           padding; 5px;
-        `
-            case "short":
-                return css`
+        `;
+      case "short":
+        return css`
           width: 120px;
           height: 40px !important;
           padding: 5px;
-        `
-            case "small":
-                return css`
+        `;
+      case "small":
+        return css`
           width: 30px;
           height: 30px !important;
           padding: 6px;
           border: 0;
           box-shadow: 0px 2px 10px #e1cccd;
-        `
-            case "percent":
-                return css`
+        `;
+      case "percent":
+        return css`
           width: 30%;
           height: 50px !important;
           padding: 6px;
@@ -74,12 +74,12 @@ const StButton = styled.button`
             width: 40%;
             margin-right: 0;
           }
-        `
-            case "round":
-                return css`
+        `;
+      case "round":
+        return css`
           width: 60px;
           height: 60px !important;
-          background-color: #fd5c63 !important;
+          background-color: ${(props) => props.bgColor}; !important;
           color: white !important;
           line-height: 1.6;
           font-size: 30px;
@@ -89,38 +89,38 @@ const StButton = styled.button`
           margin: 0 10px 0 0;
           border: 0;
           box-shadow: 0px 2px 10px #e1cccd;
-          :hover {
+          /* :hover {
             background-color: white !important;
-            color: #fd5c63 !important;
-          }
-        `
-            default:
-                return css`
+            color: #4267B2 !important;
+          } */
+        `;
+      default:
+        return css`
           width: 240px;
           height: 40px !important;
           padding: 10px;
-        `
-        }
-    }}
+        `;
+    }
+  }}
   ${({ color }) => {
-        switch (color) {
-            case "reverse":
-                return css`
+    switch (color) {
+      case "reverse":
+        return css`
           color: white;
-          background-color: #fd5c63;
-          :hover {
+          background-color: #4267b2;
+          /* :hover {
             background-color: white;
             border: 0;
-            color: #fd5c63;
-          }
-        `
-            case "line":
-                return css`
+            color: #4267b2;
+          } */
+        `;
+      case "line":
+        return css`
           background-color: white;
-          border: 1px solid #fd5c63;
-        `
-            case "tag-b":
-                return css`
+          border: 1px solid #4267b2;
+        `;
+      case "tag-b":
+        return css`
           background-color: white;
           border: 1px solid #121212;
           color: #121212;
@@ -131,19 +131,19 @@ const StButton = styled.button`
           padding: 3px;
           line-height: 0;
           height: 20px !important;
-          border-radius: 5px;
+          border-radius: 0px;
           margin: 0 3px;
           box-shadow: none;
-          :hover {
+          /* :hover {
             background-color: white;
             color: #121212;
             border: 1px solid #121212;
-          }
-        `
-            case "tag-red":
-                return css`
+          } */
+        `;
+      case "tag-red":
+        return css`
           background-color: white;
-          border: 1px solid #fd5c63;
+          border: 1px solid #4267b2;
           cursor: default;
           width: 40px;
           font-size: 13px;
@@ -151,17 +151,17 @@ const StButton = styled.button`
           padding: 3px;
           line-height: 0;
           height: 20px !important;
-          border-radius: 5px;
+          border-radius: 0px;
           margin: 0 3px;
           box-shadow: none;
-          :hover {
+          /* :hover {
             background-color: white;
-            color: #fd5c63;
-            border: 1px solid #fd5c63;
-          }
-        `
-            case "cmt-b":
-                return css`
+            color: #4267b2;
+            border: 1px solid #4267b2;
+          } */
+        `;
+      case "cmt-b":
+        return css`
           background-color: white;
           border: 1px solid #121212;
           color: #121212;
@@ -172,19 +172,19 @@ const StButton = styled.button`
           padding: 3px;
           line-height: 0;
           height: 20px !important;
-          border-radius: 5px;
+          border-radius: 0px;
           margin: 0 3px;
           box-shadow: none;
-          :hover {
+          /* :hover {
             background-color: #333;
             color: white;
             border: 1px solid #333;
-          }
-        `
-            case "cmt-red":
-                return css`
+          } */
+        `;
+      case "cmt-red":
+        return css`
           background-color: white;
-          border: 1px solid #fd5c63;
+          border: 1px solid #4267b2;
           cursor: pointer;
           width: 100%;
           font-size: 11px;
@@ -192,25 +192,25 @@ const StButton = styled.button`
           padding: 3px;
           line-height: 0;
           height: 20px !important;
-          border-radius: 5px;
+          border-radius: 0px;
           margin: 0 3px;
           box-shadow: none;
-          :hover {
-            background-color: #fd5c63;
+          /* :hover {
+            background-color: #4267b2;
             color: #white;
-            border: 1px solid #fd5c63;
-          }
-        `
-            default:
-                return css`
-          color: #fd5c63;
+            border: 1px solid #4267b2;
+          } */
+        `;
+      default:
+        return css`
+          color: #4267b2;
           background-color: white;
-          &:hover {
-            background-color: #fd5c63;
+          /* &:hover {
+            background-color: #4267b2;
             border: 0;
             color: white;
-          }
-        `
-        }
-    }}
-`
+          } */
+        `;
+    }
+  }}
+`;
